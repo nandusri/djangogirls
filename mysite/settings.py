06afa8c5f0d6cc8c6c5bb4d6f557e30c1bd7d504
nandusri <kasu.nandu@gmail.com>
2019-02-20 13:20:29 +0530

    one commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c2db6$!-%-^o(b!x@)7-no1jgb@h4752(mjdi)xx5-9&gd8n49'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
