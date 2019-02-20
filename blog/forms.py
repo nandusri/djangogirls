6d01c84eaa48a3f89a0e5d2711a686d837c4bec7
nandusri <kasu.nandu@gmail.com>
2019-02-20 13:12:49 +0530

    fully completed blog

--- blog/forms.py
+++ blog/forms.py
@@ -7,9 +7,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
-
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ('author', 'text',)        
+        fields = ('author', 'text',)
